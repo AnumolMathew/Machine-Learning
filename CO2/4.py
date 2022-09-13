221d0c611d9894a9eb536e4e43a3d14c4e30cdf7
AnumolMathew <94525732+AnumolMathew@users.noreply.github.com>
2022-09-13 16:11:08 +0530

    Add files via upload

---
+++ CO2/4.py
@@ -0,0 +1,9 @@
+import numpy as np
+print("Anumol Mathew, 007")
+
+arr = np.arange(start=1,stop=10,step=1,dtype=int)
+print(arr)
+
+print("first 4 elements: ",arr[:4])
+print("last 6 elements: ",arr[-6:])
+print("elements in index 2 to 7: ",arr[1:7])
